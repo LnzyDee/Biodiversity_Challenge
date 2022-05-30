a399ea63c3c8534406634d104e45256ffd605be9
unknown <lindsay.dove@gmail.com>
2022-05-29 22:34:41 -0500

    upload code

--- charts.js
+++ charts.js
@@ -95,5 +95,31 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = {
+      x: otuId,
+      y: sampleValues,
+      text: otuLabels,
+      mode: 'markers',
+      marker: {
+        color: otuId,
+        size: sampleValues
+      }
+   
+    };
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: 'Bacteria Cultures Per Sample',
+      xaxis: {
+        title: {
+          text: 'OTU ID'
+        }
+      }
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
   });
 }
